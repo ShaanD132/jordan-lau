54cb4612608c77683d5d628b6166ac30608cc38f
Shaan <shaanduss@gmail.com>
2022-08-07 12:56:48 +0400

    fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "80803989c8cf6883"; }
+/******/ 		__webpack_require__.h = function() { return "d7efaa9d4d38b585"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
